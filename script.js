e56f17af2de9d90ec43e682b0bfa4b6487f0415a
Ryan Headlee <headleeryan@gmail.com>
2022-05-13 09:32:23 -0700

    Create basic calculation functions

---
+++ script.js
@@ -0,0 +1,21 @@
+let result;
+
+function add(a, b) {
+  result = a + b;
+  return result;
+} 
+
+function subtract(a, b) {
+  result = a - b;
+  return result;
+}
+
+function multiply(a, b) {
+  result = a * b;
+  return result;
+}
+
+function divide(a, b) {
+  result = a / b;
+  return result;
+}
