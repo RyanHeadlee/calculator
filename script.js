4b7547dd78422fdf450efc4fc5b4c61a962ce69f
Ryan Headlee <headleeryan@gmail.com>
2022-05-13 10:14:13 -0700

    Add some event listeners

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let result;
 const info = document.querySelector('.calcInfo');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+let result;
+let a;
+let b;
 
 function add(a, b) {
   result = a + b;
@@ -25,6 +28,25 @@ function divide(a, b) {
 
 numbers.forEach(number => {
   number.addEventListener('click', () => {
-    info.textContent = number.textContent;
+    if (info.textContent == '0')  {
+      info.textContent = number.textContent;
+      return a = number.textContent;
+    }
+    info.textContent += number.textContent;
+    return a += number.textContent;
   });
 });
+
+operators.forEach(operator => {
+  operator.addEventListener('click', () => {
+    if (operator.classList.contains('add')) {
+      return equals.classList.add('add');
+    }
+  })
+})
+
+equals.addEventListener('click', () => {
+  if (equals.classList.contains('add')) {
+    return add(a, b);
+  }
+})
