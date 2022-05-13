4aa5cad32efd503f6bef8f2503b2653d9677cd1b
Ryan Headlee <headleeryan@gmail.com>
2022-05-13 10:38:53 -0700

    Make add() work on first pass through

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let b;
 
 function add(a, b) {
   result = a + b;
-  return result;
+  equals.classList.remove('add');
+  return info.textContent = result;
 } 
 
 function subtract(a, b) {
@@ -26,8 +27,26 @@ function divide(a, b) {
   return result;
 }
 
+function operate(a, b) {
+  a = Number(a);
+  b = Number(b);
+  equals.classList.remove('operate');
+  if (equals.classList.contains('add')) {
+    return add(a, b);
+  }
+}
+
 numbers.forEach(number => {
   number.addEventListener('click', () => {
+    if (equals.classList.contains('operate')) {
+      if (equals.classList.contains('first'))  {
+        equals.classList.remove('first');
+        info.textContent = number.textContent;
+        return b = number.textContent;
+      }
+      info.textContent += number.textContent;
+      return b += number.textContent;
+    }
     if (info.textContent == '0')  {
       info.textContent = number.textContent;
       return a = number.textContent;
@@ -40,13 +59,11 @@ numbers.forEach(number => {
 operators.forEach(operator => {
   operator.addEventListener('click', () => {
     if (operator.classList.contains('add')) {
-      return equals.classList.add('add');
+      return equals.classList.add('add', 'operate', 'first');
     }
   })
 })
 
 equals.addEventListener('click', () => {
-  if (equals.classList.contains('add')) {
-    return add(a, b);
-  }
+  operate(a, b);
 })
